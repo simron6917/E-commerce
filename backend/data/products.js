c07209d7f8ced9dfb891e0419fd880c52b5836b5
simron6917 <simron.shrestha@featherwebs.com>
2021-09-03 13:29:42 +0545

    ecommerce site

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,6 +1,5 @@
 const products = [
   {
-   
     name: 'Airpods Wireless Bluetooth Headphones',
     image: '/images/airpods.jpg',
     description:
@@ -8,12 +7,11 @@ const products = [
     brand: 'Apple',
     category: 'Electronics',
     price: 89.99,
-    countInStock: 10,
-    rating: 4.5,
-    numReviews: 12,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
   },
   {
-    
     name: 'iPhone 11 Pro 256GB Memory',
     image: '/images/phone.jpg',
     description:
@@ -21,12 +19,11 @@ const products = [
     brand: 'Apple',
     category: 'Electronics',
     price: 599.99,
-    countInStock: 7,
-    rating: 4.0,
-    numReviews: 8,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
   },
   {
-  
     name: 'Cannon EOS 80D DSLR Camera',
     image: '/images/camera.jpg',
     description:
@@ -34,9 +31,9 @@ const products = [
     brand: 'Cannon',
     category: 'Electronics',
     price: 929.99,
-    countInStock: 5,
-    rating: 3,
-    numReviews: 12,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
   },
   {
     name: 'Sony Playstation 4 Pro White Version',
@@ -46,12 +43,11 @@ const products = [
     brand: 'Sony',
     category: 'Electronics',
     price: 399.99,
-    countInStock: 11,
-    rating: 5,
-    numReviews: 12,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
   },
   {
- 
     name: 'Logitech G-Series Gaming Mouse',
     image: '/images/mouse.jpg',
     description:
@@ -60,11 +56,10 @@ const products = [
     category: 'Electronics',
     price: 49.99,
     countInStock: 7,
-    rating: 3.5,
-    numReviews: 10,
+    rating: 0,
+    numReviews: 0,
   },
   {
-   
     name: 'Amazon Echo Dot 3rd Generation',
     image: '/images/alexa.jpg',
     description:
@@ -73,8 +68,20 @@ const products = [
     category: 'Electronics',
     price: 29.99,
     countInStock: 0,
-    rating: 4,
-    numReviews: 12,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Sony Playstation 4 Pro White Version',
+    image: '/images/playstation.jpg',
+    description:
+      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 399.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
   },
 ]
 
